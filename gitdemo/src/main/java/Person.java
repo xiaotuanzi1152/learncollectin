91b0f1c70f64c4d4360c1409963295de4859e49b
xjt <476067384@qq.com>
2021-12-07 13:40:49 +0800

    git demo

---
+++ gitdemo/src/main/java/Person.java
@@ -0,0 +1,11 @@
+/**
+ * @program: collection1
+ * @description:
+ * @author: XTZ
+ * @create: 2021-12-07 13:39
+ **/
+public class Person {
+    private String name;
+    private int age;
+    private String address;
+}
