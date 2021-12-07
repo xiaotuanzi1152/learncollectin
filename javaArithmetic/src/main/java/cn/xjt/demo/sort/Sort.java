2eb9d7e2007363ca137fe9b0eebb117432338844
xjt <476067384@qq.com>
2021-12-07 12:23:59 +0800

    11

--- javaArithmetic/src/main/java/cn/xjt/demo/sort/Sort.java
+++ javaArithmetic/src/main/java/cn/xjt/demo/sort/Sort.java
@@ -2,7 +2,7 @@ package cn.xjt.demo.sort;
 
 /**
  *使用辅助函数 less() 和 swap() 来进行比较和交换的操作，使得代码的可读性和可移植性更好
- * @param <T>
+ * @param <T>sdsdsdsdf
  */
 
 public abstract class Sort<T extends Comparable<T>> {
